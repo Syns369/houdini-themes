72afd42e7039ba51ab44a6616135ce644c1cd253
Syns2 <synss@protonmail.com>
2021-09-15 02:13:48 +0200

    added vue.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: '/houdini-themes/',
+    base: '/houdini-themes/',
     plugins: [vue()],
     build: {
         rollupOptions: {
